cced37f66658fa037f67fafb25e2ee4c6feb6ebe
Efua Akumanyi <efua.akumanyi@gmail.com>
2022-09-05 12:54:51 +0100

    Update to task

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,12 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+var a = 5;
+var b = 10;
+var c= a + b;
+
+console.log(c);
+
+a = 20;
+
+console.log(c);
