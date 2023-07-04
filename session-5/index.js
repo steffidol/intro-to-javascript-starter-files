6c4a17dfbf5ae22014cf960331779f10495c12dd
Steffi <steffidol@gmail.com>
2023-07-04 14:15:56 -0400

    added getGrade function using if statements

--- session-5/index.js
+++ session-5/index.js
@@ -1,26 +1,43 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
-let randomNumber = Math.round(Math.random() * 10);
+let randomNumber = Math.round(Math.random() * 10
 
-// ******************************** WHILE LOOPS **************************************
+function getGrade(total_mark){
 
+    let grade = "Ungraded"
 
+    if (total_mark > 90){
+        grade = "A+";
+    }
+    else if (total_mark > 80 && total_mark <=90){
+        grade = "A";
+    }
+    else if (total_mark > 70 && total_mark <=80){
+        grade = "B";
+    }
+    else if (total_mark > 60 && total_mark <=70){
+        grade = "C";
+    }
+    else if (total_mark > 50 && total_mark <=60){
+        grade = "D";
+    }
+    else if (total_mark > 40 && total_mark <=50){
+        grade = "E";
+    }
+    else if (total_mark > 30 && total_mark <=40){
+        grade = "F";
+    }
+    else{
+        grade = "Fail"
+    }
 
+    console.log("Your grade is: " + grade)
 
+};
 
+getGrade(55);
 
-
-
-
-
-
-
-
-
-
-// ******************************** FOR LOOPS **************************************
-
-
-
+    console.log("Your grade is: " + grade)
+ 
 
 
 
